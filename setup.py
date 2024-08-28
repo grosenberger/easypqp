866b2f1a3ebe842a578d7c416523ceafad09d9cc
Dan Polasky <dpolasky@umich.edu>
2024-08-28 10:43:26 -0400

    bump to 0.1.50-rc3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='easypqp',
-    version='0.1.50-rc2',
+    version='0.1.50-rc3',
     description='EasyPQP: Simple library generation for OpenSWATH',
     long_description=long_description,
     long_description_content_type='text/markdown',
